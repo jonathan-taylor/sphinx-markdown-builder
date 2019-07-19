b03f785d6b6ec14cf976916ff6e95d5cdd947fc6
Jonathan Taylor <jonathan.taylor@stanford.edu>
2019-07-19 15:57:19 -0700

    adding curly brace for Rmarkdown

--- sphinx_markdown_builder/doctree2md.py
+++ sphinx_markdown_builder/doctree2md.py
@@ -500,7 +500,7 @@ class Translator(nodes.NodeVisitor):
         code_type = node['classes'][1] if 'code' in node['classes'] else ''
         if node.get('force_highlighting', False) and 'language' in node:
             code_type = node['language']
-        self.add('```' + code_type + '\n')
+        self.add('```{' + code_type + '}\n')
 
     def depart_literal_block(self, node):
         self._escape_text = True
